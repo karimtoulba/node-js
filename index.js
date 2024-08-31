a22d15ab9ece885a1fb05dd6cf3dd45d4fd12d2a
Karim Toulba <karim@wadipurple.com>
2024-08-31 16:26:54 +0300

    Adding EJS files support

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const app = express();
 
+// Use JSON
+app.use(express.json());
+
+// Endpoint localhost:3000/hi/
+app.get("/hi", (req, res) => {
+  res.send("You've visited Hi");
+});
+
 // Endpoint localhost:3000/hello
 app.get("/hello", function (req, res) {
   let numbers;
@@ -17,7 +25,7 @@ app.get("/findSum/:num1/:num2", function (req, res) {
   const num2 = req.params.num2;
   const total = Number(num1) + Number(num2);
   res.send(`You're in finSum endpoint!<br>The total is ${total}`);
-}); 
+});
 
 // Endpoint localhost:3000/welcome?name=Karim&age=34 (QUERY PARAMETERS)
 app.get("/welcome", function (req, res) {
@@ -26,9 +34,25 @@ app.get("/welcome", function (req, res) {
   res.send(`Name is ${name} and Age is ${age}`);
 });
 
-// Endpoint localhost:3000/hi/
-app.get("/hi", (req, res) => {
-  res.send("You've visited Hi");
+// Endpoint returning JSON response
+app.get("/try", function (req, res) {
+  res.json({
+    name: req.query.name,
+    age: req.query.age,
+    language: "Arabic",
+  });
 });
 
-app.listen(3000);
+// Endpoint returning HTML file using EJS && QUERY in url
+app.get("/ejs", function (req, res) {
+  // res.sendFile(__dirname + "/views/file.html");
+  res.render("file.ejs", {
+    name: req.query.name,
+    age: req.query.age,
+  });
+});
+
+//Listen
+app.listen(3000, function () {
+  console.log("I'm listening to port 3000");
+});
